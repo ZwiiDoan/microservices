0c25df31c77994cc522a30ff5df025b9e8747c03
zwii.doan <zwii.doan@gmail.com>
2019-01-08 10:11:24 +1100

    4.4 Registering services with Spring Eureka

--- config-server/src/main/docker/run.sh
+++ config-server/src/main/docker/run.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 echo "********************************************************"
+echo "Environment Variables"
+echo "********************************************************"
+echo "EUREKA_SERVER_PORT=$EUREKA_SERVER_PORT"
+echo "EUREKA_SERVER_URI=$EUREKA_SERVER_URI"
+
+echo "********************************************************"
+echo "Waiting for the eureka server to start on port $EUREKA_SERVER_PORT"
+echo "********************************************************"
+while ! `nc -z eureka-server  $EUREKA_SERVER_PORT`; do sleep 3; done
+echo "******* Eureka Server has started"
+
+echo "********************************************************"
 echo "Starting Config Server"
 echo "********************************************************"
-java -cp app:app/lib/* per.duyd.training.microservices.configserver.ConfigServerApplication
+java -cp app:app/lib/* \
+    -Deureka.client.serviceUrl.defaultZone=$EUREKA_SERVER_URI\
+    per.duyd.training.microservices.configserver.ConfigServerApplication
